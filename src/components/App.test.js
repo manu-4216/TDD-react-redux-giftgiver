ee2869e96cf27471f8b4e4c78747d320cba27c5f
Manuel Micu <manu4216@gmail.com>
2018-05-29 20:38:13 +0200

    Add 2 more tests and the code for them

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -7,3 +7,13 @@ const app = shallow(<App />);
 it('renders correctly', () => {
     expect(app).toMatchSnapshot();
 });
+
+it('initializes the state with an empty array', () => {
+    expect(app.state().gifts).toEqual([]);
+});
+
+it('adds a gift to the state when clicking add button', () => {
+    app.find('.btn-add').simulate('click');
+
+    expect(app.state().gifts).toEqual([{ id: 1 }]);
+});

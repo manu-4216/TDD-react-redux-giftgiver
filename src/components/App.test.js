d57539b842bb944e2d032f16372e04c4a4f72eb9
Manuel Micu <manu4216@gmail.com>
2018-05-30 00:28:36 +0200

    Start adding the gift list

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -17,3 +17,9 @@ it('adds a gift to the state when clicking add button', () => {
 
     expect(app.state().gifts).toEqual([{ id: 1 }]);
 });
+
+it('adds a new gift to the rendered list when clicking the add gift button', () => {
+    app.find('.btn-add').simulate('click');
+
+    expect(app.find('.gift-list').children().length).toEqual(2);
+});

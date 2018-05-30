7313722d98b53d1425e57c56df6c926c8d52c467
Manuel Micu <manu4216@gmail.com>
2018-05-31 00:09:23 +0200

    Add remove gift button

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -37,6 +37,13 @@ class Gift extends Component {
                         />
                     </FormGroup>
                 </Form>
+
+                <Button
+                    className="btn-remove"
+                    onClick={() => this.props.removeGift(this.props.gift.id)}
+                >
+                    Remove Gift
+                </Button>
             </div>
         );
     }

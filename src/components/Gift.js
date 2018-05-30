e7c7f4d0c63ff2a10e92f2be04a01316acd9513b
Manuel Micu <manu4216@gmail.com>
2018-05-30 22:16:20 +0200

    Add person input inside Gift

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
+import {
+    Form,
+    FormGroup,
+    FormControl,
+    ControlLabel,
+    Button
+} from 'react-bootstrap';
 
 class Gift extends Component {
+    constructor() {
+        super();
+
+        this.state = { person: '', present: '' };
+    }
     render() {
-        return <div />;
+        return (
+            <div>
+                <Form>
+                    <FormGroup>
+                        <ControlLabel>Person</ControlLabel>
+                        <FormControl
+                            onChange={event =>
+                                this.setState({ person: event.target.value })
+                            }
+                            className="input-person"
+                        />
+                    </FormGroup>
+                </Form>
+            </div>
+        );
     }
 }
 
--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -8,4 +8,22 @@ describe('Gift', () => {
     it('renders properly', () => {
         expect(gift).toMatchSnapshot();
     });
+
+    it('intitializes a person and present in `state`', () => {
+        expect(gift.state()).toEqual({ person: '', present: '' });
+    });
+
+    describe('when typing into the person input', () => {
+        const person = 'Uncle';
+
+        beforeEach(() => {
+            gift
+                .find('.input-person')
+                .simulate('change', { target: { value: person } });
+        });
+
+        it('updates the person in `state`', () => {
+            expect(gift.state().person).toEqual(person);
+        });
+    });
 });
